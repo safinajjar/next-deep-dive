ee9aa97129a22ae121069e40970f9a45acdacd01
Musti <safi.najjar1@gmail.com>
2025-02-22 17:52:58 +0100

    add archive and latest news and also by year

--- app/archive/@archive/page.jsx
+++ app/archive/@archive/page.jsx
@@ -1,9 +1,18 @@
+import { getAvailableNewsYears } from "@/lib/news";
+import Link from "next/link";
+
 export default function ArchivePage() {
+  const links = getAvailableNewsYears();
+
   return (
     <header id="archive-header">
       <nav>
         <ul>
-          <li></li>
+          {links.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
---
+++ app/archive/@latest/default.jsx
@@ -0,0 +1,13 @@
+import NewsList from "@/components/news-list";
+import { getLatestNews } from "@/lib/news";
+
+export default function LatestNewsPage() {
+  const lastNews = getLatestNews();
+
+  return (
+    <>
+      <h2>Latest News</h2>
+      <NewsList news={lastNews} />
+    </>
+  );
+}
